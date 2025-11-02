c5882f5214c74bfd5ae9fe60ca82687b1ccdbc47
bharath-gaveni <bharathgaveni@gmail.com>
2025-11-02 22:52:04 +0530

    terraform infra

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "mongodb" {
 
 resource "terraform_data" "mongodb" {
   triggers_replace = [
-    aws_instance.mongodb.id,
+    aws_instance.mongodb.id
   ]
 
      connection {
